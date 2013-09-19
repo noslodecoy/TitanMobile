a547f3faf1e2bb32e7c50bf1632260fe0ba4c3c0
noslodecoy <noslodecoy@gmail.com>
2013-09-19 19:02:51 -0400

    Forgot files

---
+++ TitanMusicPlayer/src/me/noslo/titanmobile/TitanPlayerActivity.java
@@ -0,0 +1,18 @@
+package me.noslo.titanmobile;
+
+import me.noslo.titanmobile.bll.User;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TitanPlayerActivity extends Activity {
+
+	public TitanPlayerApplication app;
+	public User user;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.app = (TitanPlayerApplication)getApplication();
+		this.user = app.user;
+	}
+}
---
+++ TitanMusicPlayer/src/me/noslo/titanmobile/TitanPlayerApplication.java
@@ -0,0 +1,29 @@
+package me.noslo.titanmobile;
+
+import me.noslo.titanmobile.bll.MediaPlayer;
+import me.noslo.titanmobile.bll.User;
+import me.noslo.titanmobile.dal.MusicLibraryDAO;
+import android.app.Application;
+
+public class TitanPlayerApplication extends Application {
+
+	public User user;
+	public MediaPlayer mediaPlayer;
+	
+	public static final String SHARED_PREFS = "me.noslo.titanmobile.PREFS";
+	
+	public void onCreate() {
+		MusicLibraryDAO.context = this;
+	}
+	
+	public boolean login( String username, String password ) {
+		if (MusicLibraryDAO.login(username, password)) {
+			user = new User();
+			user.setUserName(username);
+			mediaPlayer = new MediaPlayer(user.queue);
+			return true;
+		}
+		return false;
+	}
+	
+}
